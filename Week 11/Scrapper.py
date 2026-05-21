527a58d24ca20f21de61b46146b3455ff79c4f07
MAMOON <f2024408160@umt.edu.pk>
2026-05-21 22:32:19 +0500

    CSV FILE CREATED

--- Week 11/Scrapper.py
+++ Week 11/Scrapper.py
@@ -1,47 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-car =input("Enter manufacturer name:")
+car = input("Enter manufacturer name: ")
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
     "Referer": "https://google.com"
 }
-url=f'https://www.pakwheels.com/new-cars/pricelist/{car}'
 
-response=requests.get(url, headers=headers)
+url = f'https://www.pakwheels.com/new-cars/pricelist/{car}'
+
+response = requests.get(url, headers=headers)
+
+def save_to_csv(data, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["Car Name", "Price"])
+
+        writer.writerows(data)
+
+    print(f"Data saved successfully in {filename}")
 
 if response.status_code == 200:
-    soup=BeautifulSoup(response.text,'html.parser')
-    tables=soup.find_all('table')
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    tables = soup.find_all('table')
+
+    car_data = []
+
     for table in tables:
-        rows=table.find_all('tr')
+
+        rows = table.find_all('tr')
+
         for row in rows:
-            cols=row.find_all('td')
-            if len(cols)>=2:
-                name=cols[0].get_text()
-                price=cols[1].get_text()
+
+            cols = row.find_all('td')
+
+            if len(cols) >= 2:
+
+                name = cols[0].get_text(strip=True)
+
+                price = cols[1].get_text(strip=True)
+
                 print(f"Car Name: {name} - Price: {price}")
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
+
+                
+                car_data.append([name, price])
+
+    save_to_csv(car_data, f"{car}_cars.csv")
+
 else:
-    print("Page not available !")
-
-# function to save data on csv file
-def save_to_csv(data, filename):
-    pass
-
-
-
-
-
-
-
+    print("Page not available!")
